c52a60468cdb6006ed5420361e58f6a4ff6b8570
Jan Hruban <jan.hruban@gooddata.com>
2014-03-11 21:52:35 +0100

    Yellow active window border

--- theme/clearlooks-shadow.h
+++ theme/clearlooks-shadow.h
@@ -2,7 +2,7 @@
 static const char normbordercolor[] = "#171717"; /* not active window borders */
 static const char normbgcolor[]     = "#171717"; /* not selected tags bg, status bg */
 static const char normfgcolor[]     = "#AEAEAE"; /* not selected tags text and status text */
-static const char selbordercolor[]  = "#AEAEAE"; /* active window border */
+static const char selbordercolor[]  = "#E6F563"; /* active window border */
 static const char selbgcolor[]      = "#313131"; /* selected tags bg, taskbar bg */
 static const char selfgcolor[]      = "#AEAEAE"; /* selected tag text, taskbar text */
 static const char urgbordercolor[]  = "#A60000";

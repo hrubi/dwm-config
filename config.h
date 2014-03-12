dd6db76ec6d1562184dd4bd1b254a1c73afad8b5
Jan Hruban <jan.hruban@gooddata.com>
2014-03-13 00:09:31 +0100

    Use urxvt with 256color

--- config.h
+++ config.h
@@ -51,7 +51,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[]           = { "dmenu_run", "-b", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]            = { "urxvt", NULL };
+static const char *termcmd[]            = { "urxvt256c", NULL };
 static const char *lockcmd[]            = { "xlock", NULL };
 static const char *browsercmd[]         = { "firefox", NULL };
 
